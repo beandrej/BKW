defde502684adceebf5694e0c4cdd1325e1dedee
beandrej <beandrej@ethz.ch>
2023-12-17 12:54:00 +0100

    first cooling demand model

--- house_classes.py
+++ house_classes.py
@@ -3,142 +3,82 @@ import matplotlib.pyplot as plt
 import netCDF4 as nc
 import numpy as np
 
+
 class House:
 
-    T_in = 300
-    solar_irr = 1000
-    pr = 0.75
+    t_des = 302
+    delta_t_des = 1
 
-    def __init__(self, area, wall_area, insulation, persons, panel):
-        self.area = area #surface area
-        self.wall_area = wall_area #surface area of walls
-        self.insulation = insulation #insulation value
-        self.persons = persons #number of persons in the house
-        self.panel = panel
+    def __init__(self, A_wall, A_window, A_floor, U_wall, U_window, U_floor, height, cooling_cap):
+        self.A_wall = A_wall
+        self.A_window = A_window
+        self.A_floor = A_floor
+        self.U_wall = U_wall
+        self.U_window = U_window
+        self.U_floor = U_floor
+        self.height = height
+        self.cooling_cap = cooling_cap
 
-    def cooling_demand(self, T_out):
-        Q = self.wall_area*self.insulation*(T_out - House.T_in)*0.024 + self.persons*3
-        return Q
+    def get_delta_c(self):
+        return cp_air * self.A_floor * self.height
+
+    def get_heat_gain(self, t_out, t_in):
+        temp_term = (self.A_wall * self.U_wall + self.A_window * self.U_window + self.A_floor * self.U_floor) * (t_out - t_in)
+        solar_term = 0
+        return (temp_term + solar_term)/3600
     
-    def solar_prod(self, efficiency):
-        E = self.panel*efficiency*House.solar_irr*House.pr
-        return E        
 
+    def get_w_ac(self, t_in):
+        
+        if t_in < self.t_des - self.delta_t_des:
+            w_ac = 0
+            return w_ac
+        
+        elif t_in > self.t_des + self.delta_t_des:
+            w_ac = 1
+            return w_ac
+        
+        else:
+            w_ac = 0
+            return w_ac
 
 
+    def get_temp_diff(self, delta_t, t_out, t_in):
+        t_add = delta_t/3600 * self.get_heat_gain(t_out, t_in) / self.get_delta_c() + delta_t/3600 * self.cooling_cap * self.get_w_ac(t_in) / self.get_delta_c() 
+        return t_add
 
-plot_it = 1
+cp_air = 4.18
+cop = 3.5
 
-houseCH = House(200, 400, 0.15, 3, 1)
 
-file_name = r'weather_data.csv'
-t_data = pd.read_csv(file_name, delimiter=',')
-t_data.fillna(0, inplace=True)
 
-print(t_data)
-dataCH = t_data[['utc_timestamp', 'CH_temperature']]
+file_name = r'ch_temp_2019/temp_data_2019.csv'
+t_data = pd.read_csv(file_name, delimiter=',', header=3)
+t_data = t_data['t2m']
+temp_list = list(t_data)
+temp_list_kelvin = [i + 273.15 for i in temp_list]
 
-q_houseCH = []
-timestamp = np.arange(len(dataCH))/8650 + 1980
-print(timestamp)
+old_house = House(200, 20, 80, 2, 4.8, 1, 2.5, 50)
 
-for T in dataCH['CH_temperature']:
-    q_houseCH.append(houseCH.cooling_demand(T))
+t_series = []
+t_initial = 250
+t_series.append(t_initial)
+hg_series =[]
 
-if plot_it == True:
-    plt.plot(timestamp, q_houseCH, label='CH')
-    plt.legend()
-    plt.show()
+for idx, t in enumerate(temp_list_kelvin):
+    t_next = t_series[idx] + old_house.get_temp_diff(3600, t, t_series[idx])
+    t_series.append(t_next)
 
+print(t_series)
 
+hours_in_year = np.arange(1,8762,1)
 
 
+plt.plot(hours_in_year, t_series, marker='o', linestyle='-')
 
 
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('X on Y Plot')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# temp_data_file_name = r'data_sample.csv'
-# temp_data = pd.read_csv(temp_data_file_name, delimiter=',')
-# temp_data.dropna(inplace=True)
-# data = temp_data.iloc[:, 3000]
-
-# file_path = r'p95_Tmax_Yearly_rcp85_mean_v1.0.nc'
-# nc_file = nc.Dataset(file_path, 'r')
-
-# print(nc_file.variables.keys())
-
-# # Access the 'lat' and 'lon' variables
-# lat_variable = nc_file.variables['lat'][:]
-# lon_variable = nc_file.variables['lon'][:]
-
-# # Define the target latitude and longitude values
-# target_lat = 44
-# target_lon = 3
-
-# # Find the indices where 'lat' and 'lon' match the target values
-# lat_indices = (lat_variable == target_lat)
-# lon_indices = (lon_variable == target_lon)
-# print(nc_file.variables.keys())
-# variable_names = nc_file.variables.keys()
-# desired_data = nc_file.variables['p95_Tmax_Yearly'][:, lat_indices, lon_indices]
-# print(desired_data)
-
-# temp_nc = []
-
+plt.show()
