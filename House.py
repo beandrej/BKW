00c3e0e1ca78894fa09be856873337ac26bcfd94
beandrej <beandrej@ethz.ch>
2024-01-16 21:52:14 +0100

    Latest version, runs without heating

--- House.py
+++ House.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
 air_density = 1.293 #[kg/m^3]
 cp_air = 1005*air_density #[J/(m^3*K)]
@@ -12,7 +9,10 @@ class House:
     delta_t_des = 1
 
 
-    def __init__(self, A_wall, A_window, A_floor, U_wall, U_window, U_floor, height, cooling_cap):
+    def __init__(self, A_wall, A_window, A_floor, U_wall, U_window, U_floor, 
+                 height, cooling_cap):
+
+
         self.A_wall = A_wall #[m^2]
         self.A_window = A_window #[m^2]
         self.A_floor = A_floor #[m^2]
@@ -21,10 +21,10 @@ class House:
         self.U_floor = U_floor #[W/(m^2*K)]
         self.height = height #[m]
         self.cooling_cap = cooling_cap #[W]
-        self.ac = self.initialize_ac()
-        self.inertia = self.initialize_inertia()
+        self.ac = self.set_ac()
+        self.inertia = self.set_inertia()
 
-    def initialize_ac(self):
+    def set_ac(self):
 
         if self.t_initial < self.t_des - self.delta_t_des:
             self.ac = 0
@@ -36,20 +36,21 @@ class House:
             self.ac = 0
             return self.ac
 
-    def initialize_inertia(self): #[J/K]
+    def set_inertia(self): #[J/K]
         return cp_air * self.A_floor * self.height
 
-    def get_heat_gain_from_env(self, t_out, t_in): # [J], heat gain per timestep
+
+
+    def get_heatgain(self, t_out, t_in): # [J], heat gain per timestep
         heat_gain = (self.A_wall * self.U_wall + self.A_window * self.U_window + self.A_floor * self.U_floor) * (t_out - t_in)
         #solar_term = 0
         return heat_gain
 
-    def ac_on(self, t_in): #[0 or 1]
+    def ac_mode(self, t_in): #[0 or 1]
 
         if t_in < self.t_des - self.delta_t_des:
             self.ac = 0
             return self.ac
-
         elif t_in > self.t_des + self.delta_t_des:
             self.ac = 1
             return self.ac
@@ -57,8 +58,8 @@ class House:
             return self.ac
 
 
-    def get_temp_diff(self, timestep, t_out, t_in): #[K]
-        t_diff = (self.get_heat_gain_from_env(t_out, t_in)*timestep / self.inertia) - (
-                  self.ac_on(t_in)*self.cooling_cap*timestep/self.inertia)
+    def get_t_diff(self, timestep, t_out, t_in): #[K]
+        t_diff = (self.get_heatgain(t_out, t_in)*timestep / self.inertia) - (
+                  self.ac_mode(t_in)*self.cooling_cap*timestep/self.inertia) # PID Regler
         return t_diff
     
--- elec_dem.py
+++ elec_dem.py
@@ -7,46 +7,50 @@ from House import House
 hours_in_year = list(np.arange(0,8761,1))
 days_in_year = [i/24 for i in hours_in_year]
 t_des = House.t_des
-
+cop = 3.5
 
 # DATA IMPORT & FORMATING
 file_name = r'ren_ninja/ZRH_2019_t2m.csv'
 t_data = pd.read_csv(file_name, delimiter=',', header=3)
 t_data = t_data['t2m']
 temp_list = list(t_data)
-
 temp_list_kelvin = [i + 293.15 for i in temp_list] # HIGHER TEMP 293.15, CUZ SOLAR TERM IGNORED IN House.py
 
 
 # INITIALIZE OBJECT
 old_house = House(200, 20, 80, 2, 4.8, 1, 2.5, 0)
-old_house_with_ac = House(200, 20, 80, 2, 4.8, 1, 2.5, 3000)
+old_house_with_ac = House(200, 20, 80, 2, 4.8, 1, 2.5, 5000)
 
 t_series_old = [old_house.t_initial]
 t_series_old_ac = [old_house_with_ac.t_initial]
 
+elec_demand_ac = []
+
 #CALCULATE 
 timestep = 1
 for idx, t in enumerate(temp_list_kelvin):
-    t_next_old = t_series_old[idx] + old_house.get_temp_diff(timestep, t, t_series_old[idx])
-    t_next_old_ac = t_series_old_ac[idx] + old_house_with_ac.get_temp_diff(timestep, t, t_series_old_ac[idx])
-
+    t_next_old = t_series_old[idx] + old_house.get_t_diff(timestep, t, t_series_old[idx])
+    t_next_old_ac = t_series_old_ac[idx] + old_house_with_ac.get_t_diff(timestep, t, t_series_old_ac[idx])
 
     t_series_old.append(t_next_old)
     t_series_old_ac.append(t_next_old_ac)
 
+    if t_next_old_ac > t_des:
+        elec_demand_ac.append(old_house_with_ac.cooling_cap)
 
+print(sum(elec_demand_ac)/cop)
 
+# PLOT
+plot_it = False 
+if plot_it:  
+    plt.plot(days_in_year, t_series_old, label='No AC')
+    plt.plot(days_in_year, t_series_old_ac, label='With AC')
+    
+    plt.axhline(y=t_des, color='r', linestyle='dotted', label='T_desired')
 
+    plt.xlabel('Days in year')
+    plt.ylabel('Temperature [K]')
+    plt.title('Inside temperature of the house')
+    plt.legend()
 
-# PLOT   
-plt.plot(days_in_year, t_series_old, label='No AC')
-plt.plot(days_in_year, t_series_old_ac, label='With AC')
-plt.axhline(y=t_des, color='r', linestyle='dotted', label='T_desired')
-
-plt.xlabel('Days in year')
-plt.ylabel('Temperature [K]')
-plt.title('Inside temperature of the house')
-plt.legend()
-
-plt.show()
+    plt.show()
