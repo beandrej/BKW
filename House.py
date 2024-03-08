ee7b8fc40b60300eab287efd2496c10a02f8f12a
beandrej <beandrej@ethz.ch>
2024-03-08 16:56:42 +0100

    added solar

--- House.py
+++ House.py
@@ -4,13 +4,13 @@ cp_air = 1005*air_density #[J/(m^3*K)]
 
 class House:
 
-    t_initial = 300
-    t_des = 300
+    t_initial = 283
+    t_des = 280
     delta_t_des = 1
 
 
     def __init__(self, A_wall, A_window, A_floor, U_wall, U_window, U_floor, 
-                 height, cooling_cap):
+                 height, cooling_cap, shgc, windows_s, people):
 
 
         self.A_wall = A_wall #[m^2]
@@ -23,6 +23,9 @@ class House:
         self.cooling_cap = cooling_cap #[W]
         self.ac = self.set_ac()
         self.inertia = self.set_inertia()
+        self.shgc = shgc
+        self.people = people
+        self.windows_s = windows_s
 
     def set_ac(self):
 
@@ -41,10 +44,11 @@ class House:
 
 
 
-    def get_heatgain(self, t_out, t_in): # [J], heat gain per timestep
+    def get_heatgain(self, t_out, t_in, solar_irr): # [J], heat gain per timestep
         heat_gain = (self.A_wall * self.U_wall + self.A_window * self.U_window + self.A_floor * self.U_floor) * (t_out - t_in)
-        #solar_term = 0
-        return heat_gain
+        solar_term = self.shgc * self.windows_s * solar_irr
+        people_term = self.people * 100             # 100[W] per person
+        return heat_gain + solar_term + people_term
 
     def ac_mode(self, t_in): #[0 or 1]
 
@@ -58,8 +62,8 @@ class House:
             return self.ac
 
 
-    def get_t_diff(self, timestep, t_out, t_in): #[K]
-        t_diff = (self.get_heatgain(t_out, t_in)*timestep / self.inertia) - (
-                  self.ac_mode(t_in)*self.cooling_cap*timestep/self.inertia) # PID Regler
+    def get_t_diff(self, timestep, t_out, t_in, solar): #[K]
+        t_diff = (self.get_heatgain(t_out, t_in, solar)*timestep / self.inertia) - (
+                  self.ac_mode(t_in)*self.cooling_cap*timestep/self.inertia) #PID Regler
         return t_diff
     
--- elec_dem.py
+++ elec_dem.py
@@ -4,45 +4,54 @@ import matplotlib.pyplot as plt
 
 from House import House
 
-hours_in_year = list(np.arange(0,8761,1))
+hours_in_year = list(np.arange(0, 8761, 1))
 days_in_year = [i/24 for i in hours_in_year]
 t_des = House.t_des
 cop = 3.5
 
 # DATA IMPORT & FORMATING
-file_name = r'ren_ninja/ZRH_2019_t2m.csv'
-t_data = pd.read_csv(file_name, delimiter=',', header=3)
+T_file = r'ren_ninja/ZRH_2019_t2m.csv'
+t_data = pd.read_csv(T_file, delimiter=',', header=3)
 t_data = t_data['t2m']
 temp_list = list(t_data)
-temp_list_kelvin = [i + 293.15 for i in temp_list] # HIGHER TEMP 293.15, CUZ SOLAR TERM IGNORED IN House.py
+temp_list_kelvin = [i + 273.15 for i in temp_list] # HIGHER TEMP 293.15, CUZ SOLAR TERM IGNORED IN House.py
+
+S_file = r'ren_ninja/ninja_weather_47.3744_8.5410_uncorrected.csv'
+s_data = pd.read_csv(S_file, delimiter=',', header=3)
+s_data = s_data['swgdn']
+irr_list = list(s_data)
 
 
 # INITIALIZE OBJECT
-old_house = House(200, 20, 80, 2, 4.8, 1, 2.5, 0)
-old_house_with_ac = House(200, 20, 80, 2, 4.8, 1, 2.5, 5000)
+old_house = House(200, 20, 80, 2, 4.8, 1, 2.5, 0, 0.75, 2, 4)
+old_house_with_ac = House(200, 20, 80, 2, 4.8, 1, 2.5, 5000, 0.75, 2, 4)
 
 t_series_old = [old_house.t_initial]
 t_series_old_ac = [old_house_with_ac.t_initial]
 
-elec_demand_ac = []
+elec_demand_ac = [0]
 
 #CALCULATE 
 timestep = 1
 for idx, t in enumerate(temp_list_kelvin):
-    t_next_old = t_series_old[idx] + old_house.get_t_diff(timestep, t, t_series_old[idx])
-    t_next_old_ac = t_series_old_ac[idx] + old_house_with_ac.get_t_diff(timestep, t, t_series_old_ac[idx])
+    t_next_old = t_series_old[idx] + old_house.get_t_diff(timestep, t, t_series_old[idx], irr_list[idx])
+    t_next_old_ac = t_series_old_ac[idx] + old_house_with_ac.get_t_diff(timestep, t, t_series_old_ac[idx], irr_list[idx])
 
     t_series_old.append(t_next_old)
     t_series_old_ac.append(t_next_old_ac)
 
     if t_next_old_ac > t_des:
         elec_demand_ac.append(old_house_with_ac.cooling_cap)
+    else:
+        elec_demand_ac.append(0)
 
-print(sum(elec_demand_ac)/cop)
+print(f'Total cooling demand needed: {round(sum(elec_demand_ac)/cop/1e6, 3)}[MW]')
 
 # PLOT
-plot_it = False 
-if plot_it:  
+plot_t_series = False
+plot_demand = True
+
+if plot_t_series:
     plt.plot(days_in_year, t_series_old, label='No AC')
     plt.plot(days_in_year, t_series_old_ac, label='With AC')
     
@@ -52,5 +61,12 @@ if plot_it:
     plt.ylabel('Temperature [K]')
     plt.title('Inside temperature of the house')
     plt.legend()
+    plt.show()
 
+if plot_demand:
+    plt.plot(days_in_year, elec_demand_ac, label='Cooling demand over the year')
+    plt.xlabel('Days in year')
+    plt.ylabel('Cooling demand [W]')
+    plt.title('Cooling demand over the year')
+    plt.legend()
     plt.show()
