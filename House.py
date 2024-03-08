f6e67ae9db565d1c70cddd30d6b23a374f0f4f29
beandrej <beandrej@ethz.ch>
2024-03-08 23:04:22 +0100

    updated model with real parameters

--- House.py
+++ House.py
@@ -1,16 +1,51 @@
 
 air_density = 1.293 #[kg/m^3]
 cp_air = 1005*air_density #[J/(m^3*K)]
+t_initial = 293
+t_des = 286
+delta_t_des = 1
+ac_range = 1
+
+lower_bound = t_des - delta_t_des
+upper_bound = t_des + delta_t_des + ac_range
+
+class ACUnit:
+    def __init__(self, power):
+        self.setpoint = t_des
+        self.delta_t_des = delta_t_des
+        self.ac_state =  self.set_ac()
+        self.power = power
+
+    def set_ac(self):
+        if t_initial < lower_bound:
+            self.ac = 0
+            return self.ac
+        elif t_initial > upper_bound:
+            self.ac = 1
+            return self.ac
+        else:
+            self.ac = 0
+            return self.ac
+
+    def control(self, t_in):
+
+        if upper_bound > t_in:
+            self.ac_state = 1
+        elif lower_bound < t_in:
+            self.ac_state = 0
+        else:
+            self.ac_state = (t_in - lower_bound) / (upper_bound - lower_bound)
+        return self.ac_state
+
+
+    def is_on(self):
+        return self.ac_state == 1
+
 
 class House:
 
-    t_initial = 283
-    t_des = 280
-    delta_t_des = 1
-
-
-    def __init__(self, A_wall, A_window, A_floor, U_wall, U_window, U_floor, 
-                 height, cooling_cap, shgc, windows_s, people):
+    def __init__(self, A_wall, A_window, A_floor, U_wall, U_window, U_floor,
+                 height, cooling_cap, shgc, perc_s_windows, people, v_rate):
 
 
         self.A_wall = A_wall #[m^2]
@@ -21,49 +56,26 @@ class House:
         self.U_floor = U_floor #[W/(m^2*K)]
         self.height = height #[m]
         self.cooling_cap = cooling_cap #[W]
-        self.ac = self.set_ac()
+        self.ac = ACUnit(cooling_cap)
         self.inertia = self.set_inertia()
         self.shgc = shgc
         self.people = people
-        self.windows_s = windows_s
-
-    def set_ac(self):
-
-        if self.t_initial < self.t_des - self.delta_t_des:
-            self.ac = 0
-            return self.ac
-        elif self.t_initial > self.t_des + self.delta_t_des:
-            self.ac = 1
-            return self.ac
-        else:
-            self.ac = 0
-            return self.ac
+        self.windows_s = perc_s_windows * self.A_window
+        self.v_rate = v_rate
 
     def set_inertia(self): #[J/K]
-        return cp_air * self.A_floor * self.height
-
-
+        air_term = cp_air * self.A_floor*0.5 * self.height
+        return air_term
 
     def get_heatgain(self, t_out, t_in, solar_irr): # [J], heat gain per timestep
-        heat_gain = (self.A_wall * self.U_wall + self.A_window * self.U_window + self.A_floor * self.U_floor) * (t_out - t_in)
+        heat_gain = (self.A_wall * self.U_wall + self.A_window * self.U_window + self.A_floor * self.U_floor + self.v_rate * self.A_floor) * (t_out - t_in)
         solar_term = self.shgc * self.windows_s * solar_irr
-        people_term = self.people * 100             # 100[W] per person
-        return heat_gain + solar_term + people_term
-
-    def ac_mode(self, t_in): #[0 or 1]
-
-        if t_in < self.t_des - self.delta_t_des:
-            self.ac = 0
-            return self.ac
-        elif t_in > self.t_des + self.delta_t_des:
-            self.ac = 1
-            return self.ac
-        else:
-            return self.ac
+        people_term = self.people * 100          # 100[W] per person
+        return heat_gain + solar_term*0.5 + people_term
 
 
     def get_t_diff(self, timestep, t_out, t_in, solar): #[K]
         t_diff = (self.get_heatgain(t_out, t_in, solar)*timestep / self.inertia) - (
-                  self.ac_mode(t_in)*self.cooling_cap*timestep/self.inertia) #PID Regler
+                  self.ac.control(t_in)*self.cooling_cap*timestep / self.inertia)
         return t_diff
-    
+
--- elec_dem.py
+++ elec_dem.py
@@ -4,59 +4,74 @@ import matplotlib.pyplot as plt
 
 from House import House
 
+# GLOBAL VARIABLES
 hours_in_year = list(np.arange(0, 8761, 1))
 days_in_year = [i/24 for i in hours_in_year]
-t_des = House.t_des
+t_initial = 293
+t_des = 286
+delta_t_des = 1
 cop = 3.5
+
+
 
 # DATA IMPORT & FORMATING
 T_file = r'ren_ninja/ZRH_2019_t2m.csv'
 t_data = pd.read_csv(T_file, delimiter=',', header=3)
 t_data = t_data['t2m']
 temp_list = list(t_data)
-temp_list_kelvin = [i + 273.15 for i in temp_list] # HIGHER TEMP 293.15, CUZ SOLAR TERM IGNORED IN House.py
+temp_list_kelvin = [i + 273.15 for i in temp_list]
 
 S_file = r'ren_ninja/ninja_weather_47.3744_8.5410_uncorrected.csv'
 s_data = pd.read_csv(S_file, delimiter=',', header=3)
 s_data = s_data['swgdn']
 irr_list = list(s_data)
 
+# PARAMETERS OF HOUSES
+scenario_SFH_after_2000 = {"U_window": 1.2 , "U_wall": 0.2 ,"U_floor": 0.3, "A_window": 17 ,"A_wall":105, "A_floor":89, "shgc":0.6, "v_rate":0.7, "perc_s_window": 0.35 }
+scenario_SFH_before_2000 = {"U_window": 2.7, "U_wall":0.63,"U_floor":1.2, "A_window": 17,"A_wall":105, "A_floor":89, "shgc":0.6, "v_rate":1.4, "perc_s_window": 0.35}
+scenario_MFH_after_2000 = {"U_window": 1.3 , "U_wall": 0.2 ,"U_floor": 0.3, "A_window": 80, "A_wall":279,"A_floor":252, "shgc":0.6, "v_rate":0.7, "perc_s_window": 0.3}
+scenario_MFH_before_2000 = {"U_window": 2.8, "U_wall":0.8,"U_floor":1.3,"A_wall":279,"A_window": 80 ,"A_floor":252,"shgc":0.6, "v_rate":1.4, "perc_s_window": 0.3 }
 
 # INITIALIZE OBJECT
-old_house = House(200, 20, 80, 2, 4.8, 1, 2.5, 0, 0.75, 2, 4)
-old_house_with_ac = House(200, 20, 80, 2, 4.8, 1, 2.5, 5000, 0.75, 2, 4)
+SFH_after_2000 = House(105, 17, 89, 0.2, 1.2, 0.3, 5, 3000, 0.6, 0.35, 3, 0.7)
+SFH_before_2000 = House(105, 17, 89, 0.63, 2.7, 1.2, 5, 3000, 0.6, 0.35, 3, 1.4)
 
-t_series_old = [old_house.t_initial]
-t_series_old_ac = [old_house_with_ac.t_initial]
+T_SFH_before = [t_initial]
+T_SFH_after = [t_initial]
 
-elec_demand_ac = [0]
+elec_demand_SFH_after = [0]
+elec_demand_SFH_before = [0]
 
-#CALCULATE 
+#CALCULATE
 timestep = 1
 for idx, t in enumerate(temp_list_kelvin):
-    t_next_old = t_series_old[idx] + old_house.get_t_diff(timestep, t, t_series_old[idx], irr_list[idx])
-    t_next_old_ac = t_series_old_ac[idx] + old_house_with_ac.get_t_diff(timestep, t, t_series_old_ac[idx], irr_list[idx])
+    T_SFH_before_next = T_SFH_before[idx] + SFH_after_2000.get_t_diff(timestep, t, T_SFH_before[idx], irr_list[idx])
+    T_SFH_after_next = T_SFH_after[idx] + SFH_after_2000.get_t_diff(timestep, t, T_SFH_after[idx], irr_list[idx])
 
-    t_series_old.append(t_next_old)
-    t_series_old_ac.append(t_next_old_ac)
+    T_SFH_before.append(T_SFH_before_next)
+    T_SFH_after.append(T_SFH_after_next)
 
-    if t_next_old_ac > t_des:
-        elec_demand_ac.append(old_house_with_ac.cooling_cap)
+    if T_SFH_before_next > t_des:
+        elec_demand_SFH_before.append(SFH_before_2000.cooling_cap)
     else:
-        elec_demand_ac.append(0)
+        elec_demand_SFH_before.append(0)
 
-print(f'Total cooling demand needed: {round(sum(elec_demand_ac)/cop/1e6, 3)}[MW]')
+    if T_SFH_after_next > t_des:
+        elec_demand_SFH_after.append(SFH_after_2000.cooling_cap)
+    else:
+        elec_demand_SFH_after.append(0)
+
+print(f'Total cooling demand needed for SFH before 2000: {round(sum(elec_demand_SFH_before)/cop/1e6, 3)}[MW]')
+print(f'Total cooling demand needed for SFH after 2000: {round(sum(elec_demand_SFH_after)/cop/1e6, 3)}[MW]')
 
 # PLOT
-plot_t_series = False
-plot_demand = True
+plot_t_series = True
+plot_demand = False
 
 if plot_t_series:
-    plt.plot(days_in_year, t_series_old, label='No AC')
-    plt.plot(days_in_year, t_series_old_ac, label='With AC')
-    
+    plt.plot(days_in_year, T_SFH_before, label='SFH after 2000')
+    plt.plot(days_in_year, T_SFH_after, label='SFH before 2000')
     plt.axhline(y=t_des, color='r', linestyle='dotted', label='T_desired')
-
     plt.xlabel('Days in year')
     plt.ylabel('Temperature [K]')
     plt.title('Inside temperature of the house')
@@ -70,3 +85,5 @@ if plot_demand:
     plt.title('Cooling demand over the year')
     plt.legend()
     plt.show()
+
+print(SFH_after_2000.set_inertia())
