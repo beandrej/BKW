4dfb1bb3fbd0d156730572fe3b093336698d4f26
Fiona Turner-Hehlen <fiona.turner-hehlen@levitronix.com>
2024-03-08 16:36:05 +0100

    commented code

--- supply_model.R
+++ supply_model.R
@@ -5,16 +5,16 @@ library("dplyr")
 ## A FUNCTION THAT EXTRACTS THE HOURLY SURFACE IRRADIANCE VALUES FROM A CSV FILE FOR A GIVEN YEAR AND RETURNS THEM IN A DATA FRAME
 hourly_irradiance_from_csv <- function(dataset_filename, year) {
 
+  #read in the data from the csv file
   data <- read.csv(dataset_filename)
 
+  #filter for specific year
   yearly_data <- data %>% filter(grepl(year, time))
 
-  #print(head(yearly_data,10))
-
+  #extract time and irradiance columns
   yearly_irradiance_data <- yearly_data[c('time','irradiance_surface')]
 
-  #print(head(yearly_irradiance_data,10))
-
+  #create new data frame including the unit
   solar_data <- data.frame(
     Hours = yearly_irradiance_data[c('time')],
     Irradiance = yearly_irradiance_data[c('irradiance_surface')],
@@ -25,6 +25,6 @@ hourly_irradiance_from_csv <- function(dataset_filename, year) {
 
 total_supply <- 0
 
-hourly_irradiance <- hourly_irradiance_from_csv("ninja_weather_country_CH_merra-2_population_weighted.csv", '2019')
+hourly_irradiance <- hourly_irradiance_from_csv("ninja_weather_country_CH_merra-2_population_weighted.csv", 2019)
 
 print(head(hourly_irradiance, 150))
