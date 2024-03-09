22c8da353eada0d20d6b483e737c5d05c2844f15
LSchmidtEn <150145178+LSchmidtEn@users.noreply.github.com>
2024-03-09 14:21:04 +0100

    Update with PV

--- House.py
+++ House.py
@@ -1,26 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 air_density = 1.293 #[kg/m^3]
 cp_air = 1005*air_density #[J/(m^3*K)]
-t_initial = 293
-t_des = 286
+cc_density = 800 #[kg/m^3]
+cp_cc = 840*cc_density #[J/(m^3*K)]
+wall_th = 0.1
+
+#t_initial = 295
+#t_des = 295
 delta_t_des = 1
 ac_range = 1
+cop = 3.5
 
-lower_bound = t_des - delta_t_des
-upper_bound = t_des + delta_t_des + ac_range
+#lower_bound = t_des - delta_t_des
+#upper_bound = t_des + delta_t_des + ac_range
+#ub_lb_range = upper_bound - lower_bound
 
 class ACUnit:
-    def __init__(self, power):
-        self.setpoint = t_des
-        self.delta_t_des = delta_t_des
-        self.ac_state =  self.set_ac()
-        self.power = power
 
+    def __init__(self, power, setpoint, range, t_initial):
+        self.setpoint = setpoint
+        self.range = range
+        self.power = power
+        self.lb = self.setpoint
+        self.ub = self.setpoint + self.range
+        self.t_initial = t_initial
+        self.ac_state = self.set_ac()
     def set_ac(self):
-        if t_initial < lower_bound:
+        if self.t_initial < self.lb:
             self.ac = 0
             return self.ac
-        elif t_initial > upper_bound:
+        elif self.t_initial > self.ub:
             self.ac = 1
             return self.ac
         else:
@@ -29,23 +41,60 @@ class ACUnit:
 
     def control(self, t_in):
 
-        if upper_bound > t_in:
+        if self.ub < t_in:
             self.ac_state = 1
-        elif lower_bound < t_in:
+        elif self.lb > t_in:
             self.ac_state = 0
         else:
-            self.ac_state = (t_in - lower_bound) / (upper_bound - lower_bound)
+            self.ac_state = 0.8 + (t_in - self.lb) / (self.ub - self.lb) * 0.2
         return self.ac_state
 
 
     def is_on(self):
         return self.ac_state == 1
 
+class HeatPump:
+
+    def __init__(self, power, setpoint, range, t_initial):
+        self.power = power
+        self.setpoint = setpoint
+        self.range = range
+        self.lb = self.setpoint - self.range
+        self.ub = self.setpoint
+        self.t_initial = t_initial
+        self.hp_state = self.set_hp()
+
+
+
+    def set_hp(self):
+        if self.t_initial < self.lb:
+            self.hp = 0
+            return self.hp
+        elif self.t_initial > self.ub:
+            self.hp = 1
+            return self.hp
+        else:
+            self.hp = 0
+            return self.hp
+
+    def control(self, t_in):
+        if self.lb > t_in:
+            self.hp_state = 1
+        elif self.ub < t_in:
+            self.hp_state = 0
+        else:
+            self.hp_state = 0.8 + (t_in - self.lb) / (self.ub - self.lb) * 0.2
+        return self.hp_state
+
+    def is_on(self):
+        return self.hp_state == 1
 
 class House:
 
+
     def __init__(self, A_wall, A_window, A_floor, U_wall, U_window, U_floor,
-                 height, cooling_cap, shgc, perc_s_windows, people, v_rate):
+                 height, cooling_cap, heating_cap, shgc, perc_s_windows, people, v_rate, setpoint_ac, setpoint_hp,
+                 shade_factor=0.7, t_initial = 292, t_des=295):
 
 
         self.A_wall = A_wall #[m^2]
@@ -56,26 +105,131 @@ class House:
         self.U_floor = U_floor #[W/(m^2*K)]
         self.height = height #[m]
         self.cooling_cap = cooling_cap #[W]
-        self.ac = ACUnit(cooling_cap)
+        self.heating_cap = heating_cap #[W]
         self.inertia = self.set_inertia()
         self.shgc = shgc
         self.people = people
         self.windows_s = perc_s_windows * self.A_window
         self.v_rate = v_rate
+        self.shade_factor = shade_factor
+        self.setpoint_ac = setpoint_ac
+        self.setpoint_hp = setpoint_hp
+        self.t_dev=1
+        self.t_des = t_des
+        self.t_initial=t_initial
+        self.ac = ACUnit(cooling_cap, self.setpoint_ac, self.t_dev, self.t_initial)
+        self.hp = HeatPump(heating_cap, self.setpoint_hp, self.t_dev, self.t_initial)
 
     def set_inertia(self): #[J/K]
-        air_term = cp_air * self.A_floor*0.5 * self.height
-        return air_term
+        air_term = cp_air * self.A_floor * self.height
+        cc_term = cp_cc * self.A_wall * wall_th
+        return air_term + cc_term
 
-    def get_heatgain(self, t_out, t_in, solar_irr): # [J], heat gain per timestep
-        heat_gain = (self.A_wall * self.U_wall + self.A_window * self.U_window + self.A_floor * self.U_floor + self.v_rate * self.A_floor) * (t_out - t_in)
-        solar_term = self.shgc * self.windows_s * solar_irr
+    def get_heatgain(self, t_out, t_in, solar_irr): # [W], heat gain per timestep
+        heat_gain = (self.A_wall * self.U_wall + self.A_window * self.U_window + self.A_floor * self.U_floor + self.v_rate/3600 * self.A_floor) * (t_out - t_in)
+        solar_term = self.shgc * self.windows_s * solar_irr * (1-self.shade_factor)
         people_term = self.people * 100          # 100[W] per person
-        return heat_gain + solar_term*0.5 + people_term
+        return heat_gain + solar_term + people_term
 
 
     def get_t_diff(self, timestep, t_out, t_in, solar): #[K]
         t_diff = (self.get_heatgain(t_out, t_in, solar)*timestep / self.inertia) - (
-                  self.ac.control(t_in)*self.cooling_cap*timestep / self.inertia)
+                  self.ac.control(t_in)*self.cooling_cap*timestep / self.inertia) + (
+                  self.hp.control(t_in)*self.heating_cap*timestep / self.inertia)
         return t_diff
 
+class RunSimulation:
+    def __init__(self, house, temp_list_kelvin, irr_list, timestep,scenario={}):
+        self.house = house
+        self.temp_list_kelvin = temp_list_kelvin
+        self.irr_list = irr_list
+        self.timestep = timestep
+        self.scenario=scenario
+
+    def run_scenario(self):
+        years = self.scenario["years"]
+        increase_temp = self.scenario["temp_develop"]
+        output_dictionary = {}
+        for idx,year in enumerate(years):
+            self.temp_list_kelvin = [t+increase_temp[idx] for t in self.temp_list_kelvin] #absolute increase in temperature
+            output_dictionary[year] = self.run()
+        return output_dictionary
+
+
+
+    def run(self):
+        e_demand_list_AC = [0]
+        e_demand_list_HP=[0]
+        t_list = [self.house.t_initial]
+
+        for idx, t in enumerate(self.temp_list_kelvin):
+            t_next = t_list[idx] + self.house.get_t_diff(self.timestep, t, t_list[idx], self.irr_list[idx])
+            t_list.append(t_next)
+
+            e_demand_list_AC.append(self.house.cooling_cap * self.house.ac.ac_state)
+            e_demand_list_HP.append(self.house.heating_cap*self.house.hp.hp_state)
+
+        tot_demand_AC = sum(e_demand_list_AC)/cop
+
+
+        return t_list, e_demand_list_AC, tot_demand_AC
+
+class Plot_output:
+    def __init__(self, output_dictionary, PV_output ,t_des=295):
+        self.output = output_dictionary
+        self.hours_in_year = list(np.arange(0, 8761, 1))
+        self.days_in_year= [i/24 for i in self.hours_in_year]
+        self.t_des = t_des
+        self.PV_output=PV_output
+
+    def plot_base_case_with_PV(self):
+        years = list(self.output.keys())
+        net_demand = [a-b for a,b in zip(self.output[years[0]][1],self.PV_output)]
+        plt.plot(self.days_in_year[1:], net_demand)
+        plt.xlabel('Days in year')
+        plt.ylabel('Net Demand [W]')
+        plt.title('Net Demand')
+        plt.show()
+
+    def plot_temperature(self):
+        for year in self.output:
+            plt.plot(self.days_in_year, self.output[year][0], label='SFH after 2000 '+str(year))
+            # plt.plot(days_in_year, T_SFH_before, label='SFH before 2000')
+        plt.axhline(y=self.t_des, color='r', linestyle='dotted', label='T_desired')
+        plt.xlabel('Days in year')
+        plt.ylabel('Temperature [K]')
+        plt.title('Inside temperature of the house')
+        plt.legend()
+        plt.show()
+
+    def plot_ac_demand(self):
+        for year in self.output:
+            plt.plot(self.days_in_year, self.output[year][1], label='SFH after 2000 ' + str(year))
+            # plt.plot(days_in_year, T_SFH_before, label='SFH before 2000')
+        #plt.axhline(y=t_des, color='r', linestyle='dotted', label='T_desired')
+        plt.xlabel('Days in year')
+        plt.ylabel('Demand [W]')
+        plt.title('Cooling Demand')
+        plt.legend()
+        plt.show()
+
+    def plot_aggregated_ac_demand_over_years(self):
+        total_demand_list = []
+        years = []
+        for year in self.output:
+            total_demand_list+= [self.output[year][2]]
+            years+=[int(year)]
+        plt.plot(years, total_demand_list)
+        plt.xlabel("Year")
+        plt.ylabel("Aggregated Demand")
+        plt.title("Evolution of Cooling Demand")
+        plt.show()
+
+
+class PV:
+    def __init__(self, path):
+        self.PV_data = pd.read_csv(path, header=None)
+        self.PV_output = list(self.PV_data.iloc[:,1])
+
+    def return_PV_list(self):
+        return self.PV_output
--- elec_dem.py
+++ elec_dem.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from House import House
+from House import House, RunSimulation, ACUnit, Plot_output, PV
 
 # GLOBAL VARIABLES
 hours_in_year = list(np.arange(0, 8761, 1))
 days_in_year = [i/24 for i in hours_in_year]
-t_initial = 293
-t_des = 286
+#t_initial = 295
+#t_des = 295
 delta_t_des = 1
 cop = 3.5
 
@@ -33,44 +33,34 @@ scenario_MFH_after_2000 = {"U_window": 1.3 , "U_wall": 0.2 ,"U_floor": 0.3, "A_w
 scenario_MFH_before_2000 = {"U_window": 2.8, "U_wall":0.8,"U_floor":1.3,"A_wall":279,"A_window": 80 ,"A_floor":252,"shgc":0.6, "v_rate":1.4, "perc_s_window": 0.3 }
 
 # INITIALIZE OBJECT
-SFH_after_2000 = House(105, 17, 89, 0.2, 1.2, 0.3, 5, 3000, 0.6, 0.35, 3, 0.7)
-SFH_before_2000 = House(105, 17, 89, 0.63, 2.7, 1.2, 5, 3000, 0.6, 0.35, 3, 1.4)
+SFH_after_2000 = House(105, 17, 89, 0.2, 1.2, 0.3, 5, 4000,4000, 0.6, 0.35,3 ,0.7, 295, 293)
+MFH_before_2000 = House(279, 80, 252, 0.8, 2.8, 1.3, 5, 4000,4000, 0.6, 0.35,3 ,0.7, 295, 293)
+#SFH_before_2000 = House(105, 17, 89, 0.63, 2.7, 1.2, 5, 3000, 0.6, 0.35, 3, 1.4)
+scenario_Switzerland = {"years":["2020","2030","2040","2050"], "temp_develop":[0,0.5,0.5,0.5]}
+# SIMULATION
+#test_obj = RunSimulation(SFH_after_2000, temp_list_kelvin, irr_list, 3600)
+#test_obj = RunSimulation(MFH_before_2000, temp_list_kelvin, irr_list, 3600)
+#sfh_t_list, sfh_e_demand, tot_demand = test_obj.run()
 
-T_SFH_before = [t_initial]
-T_SFH_after = [t_initial]
+#Running scenarios
+test_obj = RunSimulation(SFH_after_2000, temp_list_kelvin, irr_list, 3600, scenario_Switzerland)
+scenario_output=test_obj.run_scenario()
+PV_generation = PV("PV_data/PVoutput_2019.csv")
+plots=Plot_output(scenario_output,PV_generation.return_PV_list())
+plots.plot_temperature()
+plots.plot_ac_demand()
+plots.plot_aggregated_ac_demand_over_years()
+plots.plot_base_case_with_PV()
 
-elec_demand_SFH_after = [0]
-elec_demand_SFH_before = [0]
-
-#CALCULATE
-timestep = 1
-for idx, t in enumerate(temp_list_kelvin):
-    T_SFH_before_next = T_SFH_before[idx] + SFH_after_2000.get_t_diff(timestep, t, T_SFH_before[idx], irr_list[idx])
-    T_SFH_after_next = T_SFH_after[idx] + SFH_after_2000.get_t_diff(timestep, t, T_SFH_after[idx], irr_list[idx])
-
-    T_SFH_before.append(T_SFH_before_next)
-    T_SFH_after.append(T_SFH_after_next)
-
-    if T_SFH_before_next > t_des:
-        elec_demand_SFH_before.append(SFH_before_2000.cooling_cap)
-    else:
-        elec_demand_SFH_before.append(0)
-
-    if T_SFH_after_next > t_des:
-        elec_demand_SFH_after.append(SFH_after_2000.cooling_cap)
-    else:
-        elec_demand_SFH_after.append(0)
-
-print(f'Total cooling demand needed for SFH before 2000: {round(sum(elec_demand_SFH_before)/cop/1e6, 3)}[MW]')
-print(f'Total cooling demand needed for SFH after 2000: {round(sum(elec_demand_SFH_after)/cop/1e6, 3)}[MW]')
+#print(f'Total cooling demand needed for SFH before 2000: {round(tot_demand)} [W]')
 
 # PLOT
-plot_t_series = True
+plot_t_series = False
 plot_demand = False
 
 if plot_t_series:
-    plt.plot(days_in_year, T_SFH_before, label='SFH after 2000')
-    plt.plot(days_in_year, T_SFH_after, label='SFH before 2000')
+    plt.plot(days_in_year, sfh_t_list, label='SFH after 2000')
+    #plt.plot(days_in_year, T_SFH_before, label='SFH before 2000')
     plt.axhline(y=t_des, color='r', linestyle='dotted', label='T_desired')
     plt.xlabel('Days in year')
     plt.ylabel('Temperature [K]')
@@ -79,11 +69,11 @@ if plot_t_series:
     plt.show()
 
 if plot_demand:
-    plt.plot(days_in_year, elec_demand_ac, label='Cooling demand over the year')
+    plt.plot(days_in_year, sfh_e_demand, label='Cooling demand over the year')
     plt.xlabel('Days in year')
     plt.ylabel('Cooling demand [W]')
     plt.title('Cooling demand over the year')
     plt.legend()
     plt.show()
 
-print(SFH_after_2000.set_inertia())
+#print(SFH_after_2000.set_inertia())
